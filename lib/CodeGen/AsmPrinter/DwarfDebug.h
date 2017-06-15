e04425043703ad933a907e9a9139034c78f46511
Eric Fiselier <eric@efcs.ca>
2017-06-14 22:29:51 -0600

    all changes

--- lib/CodeGen/AsmPrinter/DwarfDebug.h
+++ lib/CodeGen/AsmPrinter/DwarfDebug.h
@@ -130,9 +130,9 @@ public:
     assert(V.DebugLocListIndex == ~0U && !V.MInsn && "not an MMI entry");
     if (V.Var != Var) {
       Var->dump();
-      IA->dump();
+      if (IA) IA->dump();
       V.Var->dump();
-      V.IA->dump();
+      if (V.IA) V.IA->dump();
     }
     assert(V.Var == Var && "conflicting variable");
     assert(V.IA == IA && "conflicting inlined-at location");
